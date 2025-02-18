086d2293474a094e22472d638fb6d54f96700d1a
husnain572 <111906845+husnain572@users.noreply.github.com>
2025-02-18 17:33:09 +0500

    Time: 619 ms (45.45%) | Memory: 0B (100.00%) - LeetSync

---
+++ 577-employee-bonus/employee-bonus.sql
@@ -0,0 +1,4 @@
+-- Write your PostgreSQL query statement below
+select Employee.name, Bonus.bonus from Employee
+full outer join Bonus on Employee.empId=Bonus.empId
+where bonus < 1000 or bonus is null; 
