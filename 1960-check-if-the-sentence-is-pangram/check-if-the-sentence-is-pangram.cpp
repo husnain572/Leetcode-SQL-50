756fa28e67e737f72e8bc470cd71050532951595
husnain572 <111906845+husnain572@users.noreply.github.com>
2026-04-14 21:01:29 +0500

    Time: 2 ms (14.45%) | Memory: 8.8 MB (61.54%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+     set<char> st;
+     for(auto ch:sentence){
+        if(isalpha(ch)){
+            st.insert(ch);
+        }
+     }   
+     return st.size()==26;
+    }
+};
